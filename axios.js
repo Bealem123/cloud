06a5794bc55901fec5d0e5b2c6f4954b6ae0b6b1
Bealem123 <sintayehubealem411@gmail.com>
2025-01-26 16:42:33 +0300

    Update axios.js

--- axios.js
+++ axios.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 const baseURL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000" // Local backend URL for development
-    : "https://rds-y916.onrender.com"; // Remote backend URL for production
+    : "https://cloud-21y2.onrender.com"; // Remote backend URL for production
 
 // Set up the Axios instance with the dynamic base URL
 const instance = axios.create({
